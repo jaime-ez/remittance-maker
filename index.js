1b42c3e5583a866da2a58f0e53c32b8a2faf8a65
jaime-e <jechegarayz@gmail.com>
2016-08-10 11:55:30 -0400

    [task] quoteRemittanceFixedDestination

--- index.js
+++ index.js
@@ -45,6 +45,33 @@ Maker.prototype._calculateQuotationFixedSource = function (options, callback) {
   return callback(null, {success: true, quotation: result})
 }
 
+Maker.prototype._calculateQuotationFixedDestination = function (options, callback) {
+  var self = this
+
+  var marketExchangeRate = options.reverseQuotationTotalMinusExchangeFee / options.destinationAmountCents
+  var marketExchangerateActual = marketExchangeRate * (1 - self.btcInsurance)
+  var sourceAmountNoFees = options.destinationAmountCents * marketExchangerateActual / 100
+  var sourceAmountPlusDepositFee = sourceAmountNoFees / (1 - self.sourceCurrencyDepositFee)
+  var sourceAmountPlusDepositFeeAndDinexFee = _.toInteger(sourceAmountPlusDepositFee / (1 - self.dinexFee))
+  var dinexFeeTotalAmount = _.toInteger(sourceAmountPlusDepositFeeAndDinexFee - sourceAmountPlusDepositFee)
+  var sourceCurrencyDepositFeeAmount = _.toInteger(sourceAmountPlusDepositFee - sourceAmountNoFees)
+
+
+  var result = {
+    quotation: options.quotationAmountMinusExchangeFee,
+    reverseQuotation: options.reverseQuotationTotalMinusExchangeFee,
+    marketExchangeRate: marketExchangeRate,
+    marketExchangerateActual: marketExchangerateActual,
+    sourceAmount: sourceAmountNoFees,
+    sourceCurrencyDepositFeeAmount: sourceCurrencyDepositFeeAmount,
+    sourceAmountPlusDepositFee: sourceAmountPlusDepositFee,
+    dinexFeeTotalAmount: dinexFeeTotalAmount,
+    sourceAmountPlusDepositFeeAndDinexFee: sourceAmountPlusDepositFeeAndDinexFee
+  }
+
+  return callback(null, {success: true, quotation: result})
+}
+
 Maker.prototype.quoteRemittanceFixedSource = function (options, callback) {
   var self = this
 
@@ -94,6 +121,58 @@ Maker.prototype.quoteRemittanceFixedSource = function (options, callback) {
     ], callback)
   } else {
     return callback({success: false, error_type: 'sourceCurrency_invalid', statusCode: 400}, null)
+  }
+}
+
+Maker.prototype.quoteRemittanceFixedDestination = function (options, callback) {
+  var self = this
+
+  var client = new SurbtcRestClient({
+    api: self.apiUrl,
+    secret: self.apiKey
+  })
+
+  if (!options.destinationCurrency) {
+    return callback({success: false, error_type: 'destinationCurrency_required', statusCode: 400}, null)
+  }
+
+  if (!(options.destinationAmount && _.isFinite(options.destinationAmount))) {
+    return callback({success: false, error_type: 'destinationAmount_invalid', statusCode: 400}, null)
+  }
+
+  if (options.destinationCurrency === 'COP') {
+    var marketId = 'BTC-COP'
+    var type = 'Ask'
+    var reverseMarket = 'BTC-CLP'
+    var reverseType = 'Bid'
+    // convert destination amount to cents
+    options.destinationAmountCents = _.toInteger(options.destinationAmount) * 100
+    //include withdrawal fee
+    options.destinationAmountCents = options.destinationAmountCents / (1 - self.destinationCurrencyWithdrawalFee)
+    // get exchange fee
+    async.waterfall([
+      function (next) {
+        client.getExchangeFee(marketId, type, next)
+      },
+      function (exchangeFeeQuote, next) {
+        options.exchangeFeeQuote = _.toNumber(exchangeFeeQuote.fee_percentage.value) / 100
+        client.getExchangeFee(reverseMarket, reverseType, next)
+      },
+      function (exchangeFeeReverseQuote, next) {
+        options.exchangeFeeReverseQuote = _.toNumber(exchangeFeeReverseQuote.fee_percentage.value) / 100
+        client.getQuotation(marketId, type, options.destinationAmountCents, next)
+      },
+      function (quotation, next) {
+        options.quotation = quotation.quotation
+        client.getReverseQuotation(reverseMarket, reverseType, _.toNumber(options.quotation.amount), next)
+      },
+      function (reverseQuotation, next) {
+        options.reverseQuotation = reverseQuotation.reverse_quotation
+        self._calculateQuotationFixedDestination(options, next)
+      }
+    ], callback)
+  } else {
+    return callback({success: false, error_type: 'destinationCurrency_invalid', statusCode: 400}, null)
   }
 }
 
